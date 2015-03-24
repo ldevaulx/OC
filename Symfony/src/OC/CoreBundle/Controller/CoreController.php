00660e2dcdef23501c8a7bac73d9eb3b2292015f
ldevaulx <ldevaulx@hotmail.com>
2015-03-24 18:46:40 +0100

    avant exercice

--- Symfony/src/OC/CoreBundle/Controller/CoreController.php
+++ Symfony/src/OC/CoreBundle/Controller/CoreController.php
@@ -1,51 +1,34 @@
 <?php
 
-
 namespace OC\CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class CoreController extends Controller
 {
+  // La page d'accueil
   public function indexAction()
   {
-     // Notre liste d'annonce en dur
-    $listAdverts = array(
-      array(
-        'title'   => 'Recherche développpeur Symfony2',
-        'id'      => 1,
-        'author'  => 'Alexandre',
-        'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
-        'date'    => new \Datetime()),
-      array(
-        'title'   => 'Mission de webmaster',
-        'id'      => 2,
-        'author'  => 'Hugo',
-        'content' => 'Nous recherchons un webmaster capable de maintenir notre site internet. Blabla…',
-        'date'    => new \Datetime()),
-      array(
-        'title'   => 'Offre de stage webdesigner',
-        'id'      => 3,
-        'author'  => 'Mathieu',
-        'content' => 'Nous proposons un poste pour webdesigner. Blabla…',
-        'date'    => new \Datetime())
-      );
+    // On retourne simplement la vue de la page d'accueil
+    // L'affichage des 3 dernières annonces utilisera le contrôleur déjà existant dans PlatformBundle
+    return $this->get('templating')->renderResponse('OCCoreBundle:Core:index.html.twig');
 
-    // Et modifiez le 2nd argument pour injecter notre liste
-    return $this->render('OCCoreBundle:Core:index.html.twig', array(
-      'listAdverts' => $listAdverts
-      ));  
+    // La méthode raccourcie $this->render('...') est parfaitement valable
   }
 
-public function contactAction(Request $request)
+  // La page de contact
+  public function contactAction(Request $request)
   {
+    // On récupère la session depuis la requête, en argument du contrôleur
     $session = $request->getSession();
-    
-    $session->getFlashBag()->add('info', 'La page contact n\'existe pas encore...');
+    // Et on définit notre message
+    $session->getFlashBag()->add('info', 'La page de contact n’est pas encore disponible, merci de revenir plus tard.');
 
- 
-    // Puis on redirige vers la page de visualisation de cette annonce
-    return $this->redirect($this->generateUrl('oc_core_index'));
-  }}
+    // Enfin, on redirige simplement vers la page d'accueil
+    return new RedirectResponse($this->get('router')->generate('oc_core_home'));
+
+    // Les méthodes raccourcies $this->redirect($this->generateUrl('oc_core_home')); sont parfaitement valables
+  }
+}
--- Symfony/src/OC/PlatformBundle/AntiSpam/OCAntiSpam.php
+++ Symfony/src/OC/PlatformBundle/AntiSpam/OCAntiSpam.php
@@ -3,28 +3,41 @@
 
 namespace OC\PlatformBundle\Antispam;
 
-class OCAntispam
+class OCAntispam extends \Twig_Extension
 {
+  private $mailer;
+  private $locale;
+  private $minLength;
 
-	private $mailer;
-	private $locale;
-	private $minLength;
+  public function __construct(\Swift_Mailer $mailer, $locale, $minLength)
+  {
+    $this->mailer    = $mailer;
+    $this->locale    = $locale;
+    $this->minLength = (int) $minLength;
+  }
 
-	public function __construct(\Swift_Mailer $mailer, $locale, $minLength)
-	{
-		$this->mailer    = $mailer;
-		$this->locale    = $locale;
-		$this->minLength = (int) $minLength;
-	}
+  /**
+   * Vérifie si le texte est un spam ou non
+   *
+   * @param string $text
+   * @return bool
+   */
+  public function isSpam($text)
+  {
+    return strlen($text) < $this->minLength;
+  }
 
-	/**
-	* Vérifie si le texte est un spam ou non
-	*
-	* @param string $text
-	* @return bool
-	*/
-	public function isSpam($text)
-	{
-		return strlen($text) < $this->minLength;
-	}
+  // Twig va exécuter cette méthode pour savoir quelle(s) fonction(s) ajoute notre service
+  public function getFunctions()
+  {
+    return array(
+      'checkIfSpam' => new \Twig_Function_Method($this, 'isSpam')
+    );
+  }
+
+  // La méthode getName() identifie votre extension Twig, elle est obligatoire
+  public function getName()
+  {
+    return 'OCAntispam';
+  }
 }
